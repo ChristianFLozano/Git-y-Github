1a67829302a53e9e7f79bbebb8566e0dc998d635
Jhovany <marmolegomarila@gmail.com>
2025-06-04 11:14:56 -0600

    refactor: features github section

--- src/componentes/Caracteristicas.jsx
+++ src/componentes/Caracteristicas.jsx
@@ -1,20 +1,18 @@
-
-
 function Caracteristicas(props) {
     const { titulo, descripcion, imagen } = props.caracteristica;
 
     return (
-        <div className='bg-gray-100 p-4 rounded-lg shadow-lg flex flex-col items-center justify-between'>
-            <article className='text-center'>
-                <header className='mb-4'>
-                    <h2 className='text-2xl font-semibold'>{titulo}</h2>
+        <div className='bg-gray-100 p-4 rounded-lg shadow-lg flex flex-col gap-6 items-center justify-between'>
+            <article className='text-center flex flex-col gap-3'>
+                <header>
+                    <h2 className='text-3xl font-semibold'>{titulo}</h2>
                 </header>
-                <div>
-                    <p className='text-gray-700'>{descripcion}</p>
+                <div className="text-justify">
+                    <p className='text-xl text-gray-700'>{descripcion}</p>
                 </div>
             </article>
-            <figure className='mt-4'>
-                <img className='w-full h-auto rounded-lg shadow-md' src={imagen} alt={titulo} />
+            <figure className="flex w-full h-72">
+                <img className='w-full h-11/12 rounded-lg shadow-md object-cover' src={imagen} alt={titulo} />
             </figure>
         </div>
     );
